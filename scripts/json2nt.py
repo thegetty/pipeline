53ceab5b8f5f1129c02c55f508f007a5ca6b27b3
Gregory Williams <gwilliams@getty.edu>
2019-07-31 11:11:31 -0700

    Added scripts/json2nt.py.

---
+++ scripts/json2nt.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3 -B
+
+import sys
+from pathlib import Path
+import pprint
+from pyld import jsonld
+import json
+import uuid
+import re
+
+context_filename = sys.argv[1]
+fh = open(context_filename, 'r')
+ctx = json.load(fh)
+
+proc = jsonld.JsonLdProcessor()
+r = re.compile(r'_:(\S+)')
+
+for filename in sys.argv[1:]:
+	p = Path(filename)
+	with open(filename, 'r') as fh:
+		bnode_map = {}
+		input = json.load(fh)
+		del(input['@context'])
+		triples = proc.to_rdf(input, {'expandContext': ctx, 'format': 'application/n-quads'})
+		
+		bids = set(r.findall(triples))
+		for bid in bids:
+			if bid in bnode_map:
+				u = bnode_map[bid]
+			else:
+				u = f'b{uuid.uuid4()}'
+				bnode_map[bid] = u
+			triples = re.sub(f'_:{bid} ', f'_:{u} ', triples)
+
+		with open(p.with_suffix('.nt'), 'w') as out:
+			print(triples, file=out)
+			print(f'{p.with_suffix(".nt")}')

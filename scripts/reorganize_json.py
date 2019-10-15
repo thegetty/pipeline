58e29ba8484ac5733251b8a82e214fedcde7eeb1
Gregory Williams <gwilliams@getty.edu>
2019-10-15 15:49:49 -0700

    Code cleanup.

--- scripts/reorganize_json.py
+++ scripts/reorganize_json.py
@@ -5,20 +5,11 @@ For every JSON file identified in ARGV, ensure that it is located in the
 correct directory structure. If it isn't, move it to the correct directory.
 '''
 
-import os
-import csv
 import sys
 import json
-import uuid
-import pprint
-import itertools
 from pathlib import Path
-from collections import defaultdict, Counter
 
 from settings import output_file_path
-from pipeline.util import CromObjectMerger
-from cromulent.model import factory
-from cromulent import model, reader
 
 files = []
 if len(sys.argv) > 1:
@@ -34,22 +25,22 @@ else:
 for filename in files:
 	p = Path(filename)
 	with open(filename, 'r') as fh:
-		input = json.load(fh)
+		data = json.load(fh)
 		try:
-			id = input['id']
+			uu = data['id']
 		except KeyError as e:
 			print(f'*** ERROR:{filename}: {e}')
 			continue
-		if not id.startswith('urn:uuid:'):
+		if not uu.startswith('urn:uuid:'):
 			raise Exception(f"file doesn't have a valid top-level UUID: {filename}")
-		correct_partition = id[9:11]
+		correct_partition = uu[9:11]
 		if len(p.parent.name) != 2:
 			raise Exception(f"file does not appear to be in a 1-byte partition directory: {p.parent.name}")
 		correct_path = p.parent.parent.joinpath(correct_partition)
 		correct_filename = correct_path.joinpath(p.name)
 		if p != correct_filename:
 			p.replace(correct_filename)
-# 			print(f'{p} ---> {correct_filename}')
+# 			print(f'mv {p} {correct_filename}')
 		else:
 			pass
 # 			print(f'ALREADY CORRECT: {p}')

fe8c0a758efb4b75fb16ae7d7c32883dffd695a3
Gregory Williams <gwilliams@getty.edu>
2019-10-21 16:02:54 -0700

    Use urn:uuid: IRIs as graph names in scripts/json2nq.py.

--- scripts/json2nq.py
+++ scripts/json2nq.py
@@ -28,8 +28,7 @@ for filename in sys.argv[2:]:
 			continue
 		if not id.startswith('urn:uuid:'):
 			raise Exception(f"file doesn't have a valid top-level UUID: {filename}")
-		uu = id[9:]
-		gid = f'http://data.getty.edu/provenance/sales/{uu}-graph'
+		gid = id
 		del(input['@context'])
 		input = {'@id': gid, '@graph': input}
 		triples = proc.to_rdf(input, {'expandContext': ctx, 'format': 'application/n-quads'})

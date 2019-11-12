37585ead7cbbc7205143df586dadda4f5664b924
Gregory Williams <gwilliams@getty.edu>
2019-11-12 10:24:51 -0800

    Add modeling test for: objects and visual items.

--- tests/test_pir_modeling_objects.py
+++ tests/test_pir_modeling_objects.py
@@ -18,7 +18,26 @@ class PIRModelingTest_Objects(TestProvenancePipelineOutput):
 		Test that all HumanMadeObjects are linked to a corresponding VisualItem.
 		'''
 		output = self.run_pipeline('objects')
-		warnings.warn(f'TODO: implement {inspect.getframeinfo(inspect.currentframe()).function}')
+
+		# there is one object and one visual item
+		objects = output['model-object']
+		visitems = output['model-visual-item']
+		self.assertEqual(len(objects), 1)
+		self.assertEqual(len(visitems), 1)
+
+		object = next(iter(objects.values()))
+		vi = next(iter(visitems.values()))
+		
+		# the object shows the visual item
+		self.assertEqual(len(object['shows']), 1)
+		self.assertEqual(object['shows'][0]['type'], 'VisualItem')
+		shows_id = object['shows'][0]['id']
+		self.assertEqual(shows_id, vi['id'])
+		
+		# The visual item's label contains the title of the object
+		object_title = [n['content'] for n in object.get('identified_by', []) if n['classified_as'][0]['_label'] == 'Primary Name'][0]
+		vi_label = vi['_label']
+		self.assertIn(object_title, vi_label)
 
 
 if __name__ == '__main__':

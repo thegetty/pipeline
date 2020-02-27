238d5e6dced1d81caf9c09bd817f9d47b913207d
Gregory Williams <gwilliams@getty.edu>
2020-02-26 16:08:15 -0800

    Avoid modeling of some anonymous groups.

--- pipeline/projects/sales/__init__.py
+++ pipeline/projects/sales/__init__.py
@@ -93,7 +93,7 @@ class PersonIdentity:
 		self.anon_dated_re = re.compile(r'\[ANONYMOUS - (\d+)TH C[.]\]')
 		self.anon_period_re = re.compile(r'\[ANONYMOUS - (MODERN|ANTIQUE)\]')
 		self.anon_dated_nationality_re = re.compile(r'\[(\w+) - (\d+)TH C[.]\]')
-		self.anon_nationality_re = re.compile(r'\[(?!ANON)(\w+)\]')
+		self.anon_nationality_re = re.compile(r'\[(?!ANON)(\w+)\]', re.IGNORECASE)
 
 	def acceptable_person_auth_name(self, auth_name):
 		if not auth_name or auth_name in self.ignore_authnames:
@@ -298,7 +298,7 @@ class ProvenanceUtilityHelper(UtilityHelper):
 		super().__init__(project_name)
 		# TODO: does this handle all the cases of data packed into the lot_number string that need to be stripped?
 		self.shared_lot_number_re = re.compile(r'(\[[a-z]\])')
-		self.ignore_house_authnames = CaseFoldingSet(('Anonymous',))
+		self.ignore_house_authnames = CaseFoldingSet(('Anonymous', '[Anonymous]'))
 		self.csv_source_columns = ['pi_record_no', 'star_record_no', 'catalog_number']
 		self.problematic_record_uri = f'tag:getty.edu,2019:digital:pipeline:{project_name}:ProblematicRecord'
 		self.person_identity = PersonIdentity(make_shared_uri=self.make_shared_uri, make_proj_uri=self.make_proj_uri)

thegetty/pipeline

commit 6e66ddb259edb78200b8862ac4dbdc18ff350c4f

Use dict.setdefault.

pipeline/projects/sales/__init__.py
@@ -224,10 +224,9 @@ class PersonIdentity:
 				nationalities.append(data['nationality'])
 			elif isinstance(data['nationality'], list):
 				nationalities += data['nationality']
-		data['nationality'] = []
 
-		if 'referred_to_by' not in data:
-			data['referred_to_by'] = []
+		data.setdefault('nationality', [])
+		data.setdefault('referred_to_by', [])
 
 		if data.get('name_cite'):
 			cite = vocab.BibliographyStatement(ident='', content=data['name_cite'])
@@ -237,8 +236,7 @@ class PersonIdentity:
 			nationality_match = self.anon_nationality_re.match(auth_name)
 			dated_nationality_match = self.anon_dated_nationality_re.match(auth_name)
 			dated_match = self.anon_dated_re.match(auth_name)
-			if 'events' not in data:
-				data['events'] = []
+			data.setdefault('events', [])
 			if nationality_match:
 				with suppress(ValueError):
 					nationality = nationality_match.group(1).lower()
@@ -300,10 +298,8 @@ class PersonIdentity:
 				data['names'] = [(name, {'referred_to_by': [referrer]})]
 			else:
 				data['names'] = [name]
-			if 'label' not in data:
-				data['label'] = name
-		if 'label' not in data:
-			data['label'] = '(Anonymous)'
+			data.setdefault('label', name)
+		data.setdefault('label', '(Anonymous)')
 
 		if role and not role_label:
 			role_label = f'anonymous {role}'
@@ -608,8 +604,7 @@ class ProvenanceUtilityHelper(UtilityHelper):
 			if event_record:
 				n.referred_to_by = event_record
 			a['identifiers'].append(n)
-			if 'label' not in a:
-				a['label'] = name
+			a.setdefault('label', name)
 		else:
 			a['label'] = '(Anonymous)'
 

pipeline/projects/sales/lots.py
@@ -157,8 +157,7 @@ class AddAuctionOfLot(Configurable):
 			page = vocab.WebPage(ident=page_url, label=url)
 			page.digitally_carried_by = model.DigitalObject(ident=url)
 			lot.referred_to_by = page
-			if '_texts' not in data:
-				data['_texts'] = []
+			data.setdefault('_texts', [])
 			data['_texts'].append(add_crom_data(data={}, what=page))
 
 		for problem_key, problem in problematic_records.get('lots', []):
@@ -571,8 +570,7 @@ class AddAcquisitionOrBidding(Configurable):
 			owner.residence = place
 			data['_owner_locations'].append(place_data)
 
-		if '_other_owners' not in data:
-			data['_other_owners'] = []
+		data.setdefault('_other_owners', [])
 		data['_other_owners'].append(owner_record)
 
 		tx_uri = hmo.id + f'-{record_id}-Prov'
@@ -661,8 +659,7 @@ class AddAcquisitionOrBidding(Configurable):
 		if model_custody_return:
 			self.add_transfer_of_custody(data, tx, xfer_to=sellers, xfer_from=houses, sequence=2, purpose='returning')
 
-		if '_procurements' not in data:
-			data['_procurements'] = []
+		data.setdefault('_procurements', [])
 		data['_procurements'].append(tx_data)
 
 		if amnts:
@@ -771,8 +768,7 @@ class AddAcquisitionOrBidding(Configurable):
 		UNSOLD = transaction_types['unsold']
 		UNKNOWN = transaction_types['unknown']
 
-		if '_procurements' not in data:
-			data['_procurements'] = []
+		data.setdefault('_procurements', [])
 
 		sale_type = non_auctions.get(cno, 'Auction')
 		if transaction in SOLD:

pipeline/projects/sales/objects.py
@@ -262,8 +262,7 @@ class PopulateObject(Configurable):
 		auction_data = parent.get('auction_of_lot')
 		if auction_data:
 			lno = str(auction_data['lot_number'])
-			if 'identifiers' not in data:
-				data['identifiers'] = []
+			data.setdefault('identifiers', [])
 			if not lno:
 				warnings.warn(f'Setting empty identifier on {hmo.id}')
 			data['identifiers'].append(vocab.LotNumber(ident='', content=lno))

commit 8965b7e7e51cd4b79d6ba50cf3ecfdf1343be2cd

Fix bug in pipeline.projects.sales.PersonIdentity introduced in 6e66ddb.

pipeline/projects/sales/__init__.py
@@ -225,7 +225,7 @@ class PersonIdentity:
 			elif isinstance(data['nationality'], list):
 				nationalities += data['nationality']
 
-		data.setdefault('nationality', [])
+		data['nationality'] = []
 		data.setdefault('referred_to_by', [])
 
 		if data.get('name_cite'):
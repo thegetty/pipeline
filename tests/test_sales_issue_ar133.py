b9ce0a99b23acc1d1656ccf27d68d6ebe5b7f10f
mafragias <mafragias@hotmail.com>
2022-05-05 15:11:42 +0300

    refine tests https://github.com/thegetty/pipeline/pull/358

--- tests/test_sales_issue_ar133.py
+++ tests/test_sales_issue_ar133.py
@@ -3,7 +3,7 @@ import unittest
 
 from cromulent import vocab
 
-from tests import TestSalesPipelineOutput
+from tests import TestSalesPipelineOutput,classified_identifier_sets
 vocab.add_attribute_assignment_check()
 
 class PIRModelingTest_AR133(TestSalesPipelineOutput):
@@ -11,29 +11,18 @@ class PIRModelingTest_AR133(TestSalesPipelineOutput):
         '''
         AR-133: Two New Fields to be Mapped on to Textual Work from Sales Records
         '''
-        title_page_text = {'pg_sell_1_test', 'pg_sell_2_test'}
-        seller_description = {'auc_copy_seller_1_test', 'auc_copy_seller_2_test','auc_copy_seller_3_test'}
+        title_page_text = {'Amateur des Arts'}
+        seller_description = {'Hawkins; Spackman; Jessop; etc.'}
         output = self.run_pipeline('ar133')
         text_works = output['model-lo']
         text_work = text_works['tag:getty.edu,2019:digital:pipeline:REPLACE-WITH-UUID:sales#CATALOG,B-A139']
         self.assertTrue(ling_object_check(text_work,'title page text',title_page_text))
         activities = output['model-sale-activity']
-        activity = activities['tag:getty.edu,2019:digital:pipeline:REPLACE-WITH-UUID:sales#AUCTION-EVENT,B-A139']
+        activity = activities['tag:getty.edu,2019:digital:pipeline:REPLACE-WITH-UUID:sales#AUCTION-EVENT,Br-1544']
         self.assertTrue(ling_object_check(activity,'seller description',seller_description))
 
-
-
 def ling_object_check(object,type,values):
-    expected_len = len(values)
-    got_len = 0
-    for item in object['referred_to_by']:
-        if ('classified_as' in item) and item['classified_as'][0]['_label']==type:
-            if item['content'] in values:
-                got_len+=1
-    if expected_len==got_len:
-        return True
-    else:
-        return False
+    return classified_identifier_sets(object, 'referred_to_by')[type] in values
 
 if __name__ == '__main__':
     unittest.main()

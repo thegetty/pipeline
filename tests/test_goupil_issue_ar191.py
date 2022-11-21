c110594b71b912b68415b380d87e892e42061f97
Chris Papacharalampous <chrpapach1@gmail.com>
2022-11-21 17:44:49 +0200

    [AR-191] Add modeling for visual work

--- tests/test_goupil_issue_ar191.py
+++ tests/test_goupil_issue_ar191.py
@@ -22,13 +22,47 @@ class PIRModelingTest_AR191(TestGoupilPipelineOutput):
         visual_item = output["model-visual-item"][
             "tag:getty.edu,2019:digital:pipeline:REPLACE-WITH-UUID:goupil#Object,Internal,G-23884-VisItem"
         ]
+
+        # Title of the visual work
+        self.assertEqual(
+            visual_item["_label"],
+            "Visual work of “Hamlet”",
+        )    
+
+        # Classification of the visual work with ulan id
         self.assertEqual(
             visual_item["classified_as"][0]["id"],
             "http://vocab.getty.edu/aat/300033898",
         )
+
+        # Label of the classification
+        self.assertEqual(
+            visual_item["classified_as"][0]["_label"],
+            "Historie",
+        )
+
+        # Reference to visual work by the textual work
         self.assertEqual(
             visual_item["referred_to_by"][0]["id"],
             "tag:getty.edu,2019:digital:pipeline:REPLACE-WITH-UUID:goupil#Text,Book,10,Page,185,Row,15",
+        )
+
+        # Type of the identifier
+        self.assertEqual(
+            visual_item["identified_by"][0]["classified_as"][0]["id"],
+            "http://vocab.getty.edu/aat/300417193",
+        )
+
+        # Reference to visual work by the textual work
+        self.assertEqual(
+            visual_item["identified_by"][0]["referred_to_by"][0]["id"],
+            "tag:getty.edu,2019:digital:pipeline:REPLACE-WITH-UUID:goupil#Text,Book,10,Page,185,Row,15",
+        )
+
+        # Title of the visual work
+        self.assertEqual(
+            visual_item["identified_by"][0]["content"],
+            "Hamlet",
         )
 
 

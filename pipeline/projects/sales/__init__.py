9f86c339c9bf9ce0f73db09d577ddb7bc1e267a3
Mike Griniezakis <griniezakismike12@gmail.com>
2024-05-27 15:28:59 +0300

    AR-308: fix case where many evaluations are created

--- pipeline/projects/sales/__init__.py
+++ pipeline/projects/sales/__init__.py
@@ -451,7 +451,6 @@ def add_crom_price(data, parent, services, add_citations=False):
 
 	if '[?]' in data.get('price', '') or '[or]' in data.get('price', '') or '[?]' in data.get('currency', '') or '[or]' in data.get('currency', ''):
 		data['full'] = f'{data.get("price", "")} {data.get("currency", "")}'.strip()
-	data['currency'] = data.get('currency', '').strip(' [?]')
 	for k in ('price', 'est_price', 'start_price', 'ask_price'):
 		# Each data record can only have one of these. We put the decimalized
 		# value back using the same key, but the verbatim strings are just
